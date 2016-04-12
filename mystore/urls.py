d353c78e26ad5578cfada7e16973c21190fea540
Pablo Leano <pleasedontbelong@gmail.com>
2016-04-12 18:58:27 +0200

    Install DRF

--- mystore/urls.py
+++ mystore/urls.py
@@ -2,9 +2,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    # Examples:
-    # url(r'^$', 'mystore.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
+    url(r'^products/', include('products.urls')),
     url(r'^admin/', include(admin.site.urls)),
 ]
